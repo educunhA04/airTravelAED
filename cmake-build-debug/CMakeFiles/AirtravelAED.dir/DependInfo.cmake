946d15a87b2614c5a722f790bc0287d1d3ccbf84
gamicmf <margaridacmf1969@gmail.com>
2023-12-28 15:41:06 +0000

    d

---
+++ cmake-build-debug/CMakeFiles/AirtravelAED.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/magui/Documents/AED/PROJECTS/airTravelAED/Classes/airlines.cpp" "CMakeFiles/AirtravelAED.dir/Classes/airlines.cpp.o" "gcc" "CMakeFiles/AirtravelAED.dir/Classes/airlines.cpp.o.d"
+  "/home/magui/Documents/AED/PROJECTS/airTravelAED/Classes/airport.cpp" "CMakeFiles/AirtravelAED.dir/Classes/airport.cpp.o" "gcc" "CMakeFiles/AirtravelAED.dir/Classes/airport.cpp.o.d"
+  "/home/magui/Documents/AED/PROJECTS/airTravelAED/Classes/menu.cpp" "CMakeFiles/AirtravelAED.dir/Classes/menu.cpp.o" "gcc" "CMakeFiles/AirtravelAED.dir/Classes/menu.cpp.o.d"
+  "/home/magui/Documents/AED/PROJECTS/airTravelAED/Classes/read.cpp" "CMakeFiles/AirtravelAED.dir/Classes/read.cpp.o" "gcc" "CMakeFiles/AirtravelAED.dir/Classes/read.cpp.o.d"
+  "/home/magui/Documents/AED/PROJECTS/airTravelAED/main.cpp" "CMakeFiles/AirtravelAED.dir/main.cpp.o" "gcc" "CMakeFiles/AirtravelAED.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/AirtravelAED.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "AirtravelAED"
+  "AirtravelAED.pdb"
+  "CMakeFiles/AirtravelAED.dir/Classes/airlines.cpp.o"
+  "CMakeFiles/AirtravelAED.dir/Classes/airlines.cpp.o.d"
+  "CMakeFiles/AirtravelAED.dir/Classes/airport.cpp.o"
+  "CMakeFiles/AirtravelAED.dir/Classes/airport.cpp.o.d"
+  "CMakeFiles/AirtravelAED.dir/Classes/menu.cpp.o"
+  "CMakeFiles/AirtravelAED.dir/Classes/menu.cpp.o.d"
+  "CMakeFiles/AirtravelAED.dir/Classes/read.cpp.o"
+  "CMakeFiles/AirtravelAED.dir/Classes/read.cpp.o.d"
+  "CMakeFiles/AirtravelAED.dir/main.cpp.o"
+  "CMakeFiles/AirtravelAED.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AirtravelAED.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
